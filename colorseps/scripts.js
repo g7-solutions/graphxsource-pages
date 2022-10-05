fa50b1c8600f374f0f210fd14f93b7d4a4a5a452
EvreuxPendragon <christian.rivera.lara@gmail.com>
2022-10-05 11:13:06 -0600

    Color Seps Website Update to V2

--- colorseps/scripts.js
+++ colorseps/scripts.js
@@ -1,24 +1,20 @@
 let prev = $(".prev-btn");
 let next = $(".next-btn");
-let machine = $(".machine");
+let machine = $("#seps-machine");
 let slider = $(".slider");
 let machineRotation = 0;
 let imgArray = ['A', 'B', 'C', 'D', 'E', 'F'];
 
 next.click(function () {
-    $(".reveal-box").fadeTo(200, 0, function () {
-        machineRotation += 120;
-        machine.css({ 'transform': 'rotate(' + machineRotation + 'deg)' });
-        arrayRotate(imgArray);
-    }).fadeTo(500, 1);
+    machineRotation += 90;
+    machine.css({ 'rotate': machineRotation + 'deg' });
+    arrayRotate(imgArray);
 });
 
 prev.click(function () {
-    $(".reveal-box").fadeTo(200, 0, function () {
-        machineRotation -= 120;
-        machine.css({ 'transform': 'rotate(' + machineRotation + 'deg)' });
-        arrayRotate(imgArray, true);
-    }).fadeTo(500, 1);
+    machineRotation -= 90;
+    machine.css({ 'rotate': machineRotation + 'deg' });
+    arrayRotate(imgArray, true);
 });
 
 function arrayRotate(arr, reverse) {
@@ -27,14 +23,14 @@ function arrayRotate(arr, reverse) {
     return arr;
 }
 
-slider.on("input", function(){
-    if(this.value <= 100){
-        $(".reveal-box img:nth-child(4)").css({'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + this.value + '%, 0%' + this.value + '%)'});
-    }else if(this.value >= 100 && this.value <= 200){
-        $(".reveal-box img:nth-child(3)").css({'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 100) + '%, 0%' + (this.value - 100) + '%)'});
-    }else if(this.value >= 200 && this.value <= 300){
-        $(".reveal-box img:nth-child(2)").css({'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 200) + '%, 0%' + (this.value - 200) + '%)'});
-    }else if(this.value >= 301){
-        $(".reveal-box img:nth-child(1)").css({'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 300) + '%, 0%' + (this.value - 300) + '%)'});
+slider.on("input", function () {
+    if (this.value <= 100) {
+        $(".reveal-box img:nth-child(4)").css({ 'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + this.value + '%, 0%' + this.value + '%)' });
+    } else if (this.value >= 100 && this.value <= 200) {
+        $(".reveal-box img:nth-child(3)").css({ 'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 100) + '%, 0%' + (this.value - 100) + '%)' });
+    } else if (this.value >= 200 && this.value <= 300) {
+        $(".reveal-box img:nth-child(2)").css({ 'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 200) + '%, 0%' + (this.value - 200) + '%)' });
+    } else if (this.value >= 301) {
+        $(".reveal-box img:nth-child(1)").css({ 'clip-path': 'polygon(0 100%, 100% 100%, 100% ' + (this.value - 300) + '%, 0%' + (this.value - 300) + '%)' });
     }
 });
