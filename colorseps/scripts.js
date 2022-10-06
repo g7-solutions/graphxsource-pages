38fa8083acbd0499aa66d2b9e7a7414216c5fefc
EvreuxPendragon <christian.rivera.lara@gmail.com>
2022-10-06 15:27:17 -0600

    Completed Color Seps Website
    - Fully Functional Screen Printing Machine Animation
    - Fully Functional Reveal Gallery
    - Fully Functional Sliders

--- colorseps/scripts.js
+++ colorseps/scripts.js
@@ -3,15 +3,14 @@ let next = $(".next-btn");
 let machine = $("#seps-machine");
 let slider = $(".slider");
 let machineRotation = 0;
-let current = 0;
+let current = 1;
 
 let seps = [
-    { Name: "SEP-01", Count: 9 },
-    { Name: "SEP-02", Count: 9 },
-    { Name: "SEP-03", Count: 12 },
-    { Name: "SEP-04", Count: 4 },
-    { Name: "SEP-05", Count: 6 },
-    { Name: "SEP-06", Count: 16 }
+    { Name: "shark", Count: 9 },
+    { Name: "fest", Count: 9 },
+    { Name: "eren", Count: 12 },
+    { Name: "grim", Count: 4 },
+    { Name: "cars", Count: 10 }
 ];
 
 next.click(function () {
@@ -19,35 +18,66 @@ next.click(function () {
     machine.css({ 'rotate': machineRotation + 'deg' });
     arrayRotate(seps);
     prepareRange();
-    populateMachine();
+    populateMachine(seps[0], current);
 });
 
 prev.click(function () {
     machineRotation -= 90;
     machine.css({ 'rotate': machineRotation + 'deg' });
-    arrayRotate(seps, true);
     prepareRange();
+    arrayRotate(seps, true);
+    populateMachine(seps[0], current);
 });
 
-function prepareRange(){
-    slider.attr("max", seps[0].Count - 1);
-    slider.attr("value", 0);
+function prepareRange() {
+    slider.attr("max", seps[0].Count);
+    slider.val(1);
+    slider.trigger('input');
 }
 
 function arrayRotate(arr, reverse) {
-    if (reverse) arr.unshift(arr.pop());
-    else arr.push(arr.shift());
+    if (reverse) {
+        arr.unshift(arr.pop());
+        current = current == 1 ? 4 : current -= 1;
+    }
+    else {
+        arr.push(arr.shift());
+        current = current == 4 ? 1 : current += 1;
+    }
     return arr;
 }
 
-function populateMachine(){
-    for (i = 0; i < seps[0].Count; i++){
-        $($("#arm-1 image")[i]).attr("href", "./img/");
+function populateMachine(separations, armIndex) {
+    let n = separations.Count + 1;
+    let arm = "#arm-" + armIndex + " image";
+    for (i = 0; i < separations.Count; i++) {
+        n--;
+        let href = "./img/" + separations.Name + "/" + separations.Name + "-" + (n) + ".png";
+        $($(arm)[i + (12 - separations.Count)]).attr("href", href)
+            .attr("data-status", "inactive");
     }
 }
 
-slider.on("input", function () {
-    
+$(document).ready(function () {
+    prepareRange();
+    populateMachine(seps[0], 1);
 });
 
-//$("#arm-1 .img1").css({ "clip-path": "polygon(0 " + clip + "%, 100% " + clip + "%, 100% 100%, 0% 100%)" });
+let oldValue = Number(1);
+slider.on("input", function () {
+    // console.log("----------------------");
+    // console.log("Old Value: " + oldVal);
+    let newValue = Number(slider.val());
+    if (newValue >= oldValue) {
+        let obj = "#arm-" + current + " .img" + (newValue - 1);
+        $(obj).attr("data-status", "active");
+    }
+    else {
+        let obj = "#arm-" + current + " .img" + (newValue);
+        $(obj).attr("data-status", "inactive");
+    }
+    oldValue = newValue;
+
+    // console.log("New Value: " + val);
+    // console.log("----------------------");
+});
