a1f680f5296f34e6784387517e2803da1a00d1a5
Christian Rivera <christian.rivera@graphxsource.com>
2024-12-03 11:41:32 -0600

    remove unused CSS, JavaScript, HTML files and social media SVG icons

--- gxs-developers/backend.js
+++
@@ -1,9 +0,0 @@
-/*
-    Recomendations: Keep your code clean and easy to read
-    Comment your code
-    Use console.log() to debug your code
-    Use const to declare variables
-    Reccomended Time API: http://worldtimeapi.org/
-    Ussage: http://worldtimeapi.org/api/timezone/America/Tegucigalpa
-    Any questions feel free to hit me up. Good luck!
-*/
